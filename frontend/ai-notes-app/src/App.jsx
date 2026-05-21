c0e8e13327280b17c19264ebc55476365763078e
Gary Aleño <cjbonillo4@gmail.com>
2026-05-21 17:12:02 +0800

    protect routes ensuring only those with jwt can access dashboard

--- frontend/ai-notes-app/src/App.jsx
+++ frontend/ai-notes-app/src/App.jsx
@@ -1,71 +1,3 @@
-// import { useEffect, useState } from "react";
-
-// import AddNote from "./components/AddNote";
-// import NotesList from "./components/NotesList";
-// import Filters from "./components/Filters";
-// import Quiz from "./components/Quiz";
-
-// import "./index.css";
-
-// function App() {
-//   const [notes, setNotes] = useState([]);
-//   const [category, setCategory] = useState("");
-//   const [timeFilter, setTimeFilter] = useState("");
-
-//   const fetchNotes = async () => {
-//     let url = "http://localhost:3000/notes";
-
-//     const params = new URLSearchParams();
-
-//     if (category) {
-//       params.append("category", category);
-//     }
-
-//     if (timeFilter) {
-//       params.append("time", timeFilter);
-//     }
-
-//     if (params.toString()) {
-//       url += `?${params.toString()}`;
-//     }
-
-//     const res = await fetch(url);
-
-//     const data = await res.json();
-
-//     setNotes(data);
-//   };
-
-//   useEffect(() => {
-//     fetchNotes();
-//   }, []);
-
-//   return (
-//     <div className="app">
-//       <h1>AI Notes Quiz App</h1>
-
-//       <AddNote fetchNotes={fetchNotes} />
-
-//       <Filters
-//         category={category}
-//         setCategory={setCategory}
-//         timeFilter={timeFilter}
-//         setTimeFilter={setTimeFilter}
-//         fetchNotes={fetchNotes}
-//       />
-
-//       <Quiz notes={notes} />
-
-//       <NotesList
-//         notes={notes}
-//         fetchNotes={fetchNotes}
-//       />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import {
   BrowserRouter,
   Routes,
@@ -76,6 +8,8 @@ import {
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -98,7 +32,11 @@ function App() {
 
         <Route
           path="/dashboard/*"
-          element={<Dashboard />}
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
         />
       </Routes>
     </BrowserRouter>
